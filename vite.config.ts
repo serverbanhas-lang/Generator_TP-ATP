3c231f16f1c12bba8173d962836baeb61fbd53e6
serverbanhas-lang <serverbanhas@gmail.com>
2026-06-01 19:17:06 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: './', // 👈 Menggunakan './' agar path otomatis menyesuaikan di GitHub Pages tanpa perlu tulis nama repo
       server: {
         port: 3000,
         host: '0.0.0.0',
